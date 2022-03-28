849fccffd09c6df9d08109d03ccab331162668a5
Amanda Nunes <77729852+mandyHellz@users.noreply.github.com>
2022-03-28 14:42:28 -0300

    chore: re-builded dist

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
 
 export default {
-    input: "packages/**",
+    input: "packages/fetchpokeapi-package/src/index.tsx",
     output: [
         {
             file: pkg.main,
